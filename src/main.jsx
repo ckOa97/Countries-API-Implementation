482657ae16790c7e890af223c2f998ea057e46b9
Aleksandar Stankovic <aca97.vranje@gmail.com>
2024-06-28 20:04:15 +0200

    v1.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
 import './css/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/Countries-API-Implementation">
+    <BrowserRouter basename="/Countries-API-Implementation">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
